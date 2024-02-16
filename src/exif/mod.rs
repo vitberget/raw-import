ffdc26e22164ea1f5668055e6aa9d742c791dcb1
Kenneth Hedman <github@vitberget.se>
2024-02-16 13:49:00 +0100

    size

--- src/exif/mod.rs
+++ src/exif/mod.rs
@@ -10,15 +10,19 @@ pub(crate) mod date_time;
 #[derive(Debug)]
 pub(crate) struct DirEntryWithExif {
     pub(crate) entry: DirEntry,
-    pub(crate) date_time: ExifDateTime 
+    pub(crate) date_time: ExifDateTime,
+    pub(crate) size: u64,
 }
 
 pub(crate) fn enhance_with_exif(entry: DirEntry) -> anyhow::Result<DirEntryWithExif> {
     let meta = Metadata::new_from_path(entry.path())?;
     let date_time = get_date_time(&meta)?;
 
+    let size = entry.metadata()?.len();
+
     Ok(DirEntryWithExif { 
         entry,
-        date_time
+        date_time,
+        size
     })
 }
